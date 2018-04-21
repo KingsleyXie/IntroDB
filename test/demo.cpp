5302a77d40359f15a2290c711c483eb44b186db4
Kingsley <kingsley.xym@gmail.com>
2018-04-22 02:31:36 +0800

    Prepare More Unit Test Methods

--- test/demo.cpp
+++ test/demo.cpp
@@ -2,14 +2,20 @@
 #include "catch.hpp"
 
 unsigned int Factorial( unsigned int number ) {
-	if (number == 3)
-		throw "test";
+	if (number == 0)
+		throw "excep";
 	return number <= 1 ? number : Factorial(number-1)*number;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-	REQUIRE( Factorial(1) == 1 );
-	REQUIRE( Factorial(2) == 2 );
-	REQUIRE( Factorial(3) == 6 );
-	REQUIRE( Factorial(10) == 3628800 );
+	SECTION( "Exception" ) {
+		REQUIRE_THROWS( Factorial(0), "excep");
+	}
+
+	SECTION( "Common" ) {
+		REQUIRE( Factorial(1) == 1 );
+		REQUIRE( Factorial(2) == 2 );
+		REQUIRE( Factorial(3) == 6 );
+		REQUIRE( Factorial(10) == 3628800 );
+	}
 }
