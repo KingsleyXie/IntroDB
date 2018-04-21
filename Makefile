4680179d8b878c9853fa4878adb4d8393feea401
Kingsley <kingsley.xym@gmail.com>
2018-04-22 02:45:52 +0800

    Add JSON Library With Test

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 # Test Environment: gcc version 5.2.1, Windows 10
 
-demo: test/catch.hpp test/demo.cpp
+demo-test: test/catch.hpp test/demo.cpp
 	@g++ -std=c++11 -o test/demo test/demo.cpp && ./test/demo
+
+json-test: test/catch.hpp test/json.cpp db/json.hpp
+	@g++ -std=c++11 -o test/json test/json.cpp && ./test/json
---
+++ test/json.cpp
@@ -0,0 +1,28 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../db/json.hpp"
+
+TEST_CASE( "JSON Test" ) {
+	nlohmann::json data;
+
+	data["foo"] = "bar";
+
+	REQUIRE( data["foo"] == "bar" );
+
+	data = {
+		{"pi", 3.141},
+		{"happy", true},
+		{"name", "Niels"},
+		{"nothing", nullptr},
+		{"answer", {
+			{"everything", 42}
+		}},
+		{"list", {1, 0, 2}},
+		{"object", {
+			{"currency", "USD"},
+			{"value", 42.99}
+		}}
+	};
+
+	REQUIRE( data["object"]["value"] == 42.99 );
+}
