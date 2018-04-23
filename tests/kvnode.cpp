1a8813e6fb013791af03f8a5eaee7ca8a01fa56a
Kingsley <kingsley.xym@gmail.com>
2018-04-24 01:41:47 +0800

    :boom: Write Initial KVNode B-Tree Unit Test

--- tests/kvnode.cpp
+++ tests/kvnode.cpp
@@ -5,8 +5,10 @@
 using json = nlohmann::json;
 
 #include "../db/kvnode.hpp"
+#include "../db/btree.hpp"
 
-TEST_CASE( "KVNode Operator Test" ) {
+TEST_CASE( "KVNode Operator Test" )
+{
 	json data;
 	data["foo"] = "bar";
 	KVNode nd1(1, data);
@@ -21,4 +23,38 @@ TEST_CASE( "KVNode Operator Test" ) {
 	REQUIRE( nd1 < nd2 );
 	REQUIRE( nd3 >= nd4 );
 	REQUIRE( nd1 == nd4 );
+}
+
+TEST_CASE( "KVNode B-Tree Test" )
+{
+	BTree<KVNode> kvt(3);
+
+	SECTION( "Empty Tree Test" )
+	{
+		REQUIRE_THROWS_WITH(
+			kvt.remove(1),
+			"The tree is empty!\n"
+		);
+	}
+
+	SECTION( "Operation Test" )
+	{
+		json data;
+		data["foo"] = "bar";
+
+		REQUIRE_NOTHROW( kvt.insert(1, data) );
+		REQUIRE_NOTHROW( kvt.traverse() );
+
+		data["test"] = "test";
+		REQUIRE_NOTHROW( kvt.insert(2, data) );
+		REQUIRE_NOTHROW( kvt.traverse() );
+
+		REQUIRE_NOTHROW( kvt.remove(1) );
+		REQUIRE_NOTHROW( kvt.traverse() );
+
+		REQUIRE_THROWS_WITH(
+			kvt.remove(233),
+			"Provided key is not in the tree!\n"
+		);
+	}
 }
