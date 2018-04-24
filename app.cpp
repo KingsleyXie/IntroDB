585f06f33df8fe2c3c415d5569c72c32e58f092a
Kingsley <kingsley.xym@gmail.com>
2018-04-24 17:45:36 +0800

    :sparkles: Add Operation JSON Handler For App Code

--- app.cpp
+++ app.cpp
@@ -2,11 +2,31 @@
 
 int main(int argc, char const *argv[])
 {
-	json req, res;
-	Db db("db_name");
+	if (argc != 2)
+		std::cerr << "\n\tUsage: ./app operation(json-encoded)\n";
+	else
+	{
+		json operation, data;
+		std::string db_name, tb_name, op_type;
 
-	db.restore("test.json");
-	db.traverse();
+		try {
+			operation = json::parse(argv[1]);
 
+			db_name = operation["db_name"];
+			tb_name = operation["tb_name"];
+			op_type = operation["op_type"];
+		} catch (nlohmann::detail::parse_error) {
+			std::cerr << "Request Parse Failed.\n";
+			return 0;
+		} catch (nlohmann::detail::type_error) {
+			std::cerr << "Missing necessary parameters.\n";
+			return 0;
+		}
+
+		Db db(db_name);
+
+		db.restore(db_name + ".json");
+		db.traverse();
+	}
 	return 0;
 }
